e042a748c0c35e842eb6cdd3a5c68cf561c49850
Perry Tsang <perry.tsang.98@gmail.com>
2020-02-19 23:31:48 -0500

    Added file for database

---
+++ database.py
@@ -0,0 +1,16 @@
+import sqlite3 as sql
+import pandas as pd
+
+def readData():
+    # TODO: 1. Read in data, split according to table schema
+    #       2. Add data to tables only if data is new
+
+    pass
+
+def readChanges():
+    # TODO: 1. Read in data
+    #       2. Determine severity of changes
+    #       3. Change data to code table if update only
+    #       4. Insert data into changelog table
+
+    pass
