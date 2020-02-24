e7190a7759da5b4e59b6ba54e047319281f9839d
Perry Tsang <perry.tsang.98@gmail.com>
2020-02-24 10:33:03 -0500

    Added file for database

--- database.py
+++ database.py
@@ -1,6 +1,50 @@
 import sqlite3 as sql
 import pandas as pd
 
+def createTables():
+    conn = sql.connect('CDISC.db')
+    c = conn.cursor()
+
+    c.execute('CREATE TABLE IF NOT EXISTS Code('
+              'Code TEXT,'
+              'Term_Type TEXT,'
+              'Creation_Date TEXT,'
+              'Deprecated_Date TEXT,'
+              'PRIMARY KEY (Code))')
+
+    c.execute('CREATE TABLE IF NOT EXISTS Codelist('
+              'Code TEXT,'
+              'Extensible TEXT,'
+              'Name TEXT,'
+              'Submission_Value TEXT,'
+              'Synonyms TEXT,'
+              'Definition TEXT,'
+              'NCI_Preferred_Term TEXT,'
+              'PRIMARY KEY (Code))')
+
+    c.execute('CREATE TABLE IF NOT EXISTS Term('
+              'Codelist TEXT,'
+              'Code TEXT,'
+              'Name TEXT,'
+              'Submission_Value TEXT,'
+              'Synonyms TEXT,'
+              'Definition TEXT,'
+              'NCI_Preferred_Term TEXT,'
+              'PRIMARY KEY (Codelist, Code)'
+              'FOREIGN KEY (Codelist) REFERENCES Codelist (Code))')
+
+    c.execute('CREATE TABLE IF NOT EXISTS Changes('
+              'Date TEXT,'
+              'Code TEXT,'
+              'Request_Code TEXT,'
+              'Change_Type TEXT,'
+              'Severity TEXT,'
+              'Change_Summary TEXT,'
+              'Original TEXT,'
+              'New TEXT,'
+              'PRIMARY KEY (Date, Code)'
+              'FOREIGN KEY (Code) REFERENCES Code (Code))')
+
 def readData():
     # TODO: 1. Read in data, split according to table schema
     #       2. Add data to tables only if data is new
