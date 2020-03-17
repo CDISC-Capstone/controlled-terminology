238b71428a943aea304c43488c32dd1e9c529e51
Perry Tsang <perry.tsang.98@gmail.com>
2020-03-17 15:17:53 -0400

    Generalized code to allow for loading in any standard

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ def create_tables():
     c.execute('CREATE TABLE IF NOT EXISTS Code('
               'Code TEXT,'
               'Term_Type TEXT,'
-              'Format TEXT,'
+              'Standard TEXT,'
               'Creation_Date TEXT,'
               'Current_Version TEXT,'
               'Deprecation_Date TEXT,'
@@ -58,7 +58,7 @@ def create_tables():
     conn.close()
 
 
-def read_data(date, filePath):
+def read_data(date, filePath, standard):
     connection = sql.connect('CDISC.db')
     data = pd.read_csv(filePath, sep='\t', engine='python')
 
@@ -79,8 +79,8 @@ def read_data(date, filePath):
         # Inserts codelist into table
         try:
             # Insert into Code table if not already in table
-            connection.execute('INSERT INTO Code(Code, Term_Type, Creation_Date, Current_Version, Deprecation_Date)'
-                               'VALUES (?, ?, ?, ?, ?);', (code, "CDISC Codelist", date, date, None))
+            connection.execute('INSERT INTO Code(Code, Term_Type, Standard, Creation_Date, Current_Version, Deprecation_Date)'
+                               'VALUES (?, ?, ?, ?, ?, ?);', (code, "CDISC Codelist", standard, date, date, None))
 
             # Insert into Codelist table if not already in table
             connection.execute('INSERT INTO Codelist(Code, Extensible, Name, Submission_Value, Synonyms, Definition, NCI_Preferred_Term)'
@@ -123,8 +123,8 @@ def read_data(date, filePath):
         # Inserts term into table
         try:
             # Insert into Code table if not already in table
-            connection.execute('INSERT INTO Code(Code, Term_Type, Creation_Date, Current_Version, Deprecation_Date)'
-                               'VALUES (?, ?, ?, ?, ?);', (code, "Term", date, date, None))
+            connection.execute('INSERT INTO Code(Code, Term_Type, Standard, Creation_Date, Current_Version, Deprecation_Date)'
+                               'VALUES (?, ?, ?, ?, ?, ?);', (code, "Term", standard, date, date, None))
 
             # Insert into Term table if not already in table
             connection.execute('INSERT INTO Term(Codelist, Code, Submission_Value, Synonyms, Definition, NCI_Preferred_Term)'
@@ -231,79 +231,79 @@ def read_changes(date, filePath):
     connection.close()
 
 
-# This function gets all packages from a CDISC archive link
-# The default archive link is for the SDTM archive
-def get_packages(firstPackageDate, archiveLink="https://evs.nci.nih.gov/ftp1/CDISC/SDTM/Archive/"):
+# This function gets all packages for a given standard
+def get_packages(firstPackageDate, standard):
+    # Determine archive link based on standard
+    if (standard == "SDTM") or (standard == "CDASH"):
+        archiveLink = "https://evs.nci.nih.gov/ftp1/CDISC/SDTM/Archive/"
+    else:
+        archiveLink = ""
+
     # Extracts archive HTML for BeautifulSoup
     archiveHTML = requests.get(archiveLink).text
     soup = BeautifulSoup(archiveHTML, 'html.parser')
 
-    SDTMPackages = []
-    SDTMChanges = []
-    # Finds all SDTM links
+    packages = []
+    changes = []
+    # Finds all links for that standard
     for link in soup.find_all('a'):
-        # Finds all link references and tests if they are SDTM in .txt format
+        # Finds all link references and tests if they are of that format in .txt format
         end = link.get('href')
-        SDTM = re.match(r"^SDTM%20Terminology\S*.txt$", end)
+        txt = re.match(r"^{}%20Terminology\S*.txt$".format(standard), end)
 
         # If it is a SDTM .txt link, split between package and changelist
-        if SDTM:
+        if txt:
             fullURL = archiveLink + end
-            # Uses "Change" instead of "Changes" because of a typo in the link for 2016-09-30 changelist
-            change = re.search("Change", end)
-
-            if change:
-                SDTMChanges.append(fullURL)
+            # Uses "Change" instead of "Changes" because of a typo in the link for 2016-09-30 SDTM changelist
+            if "Change" in end:
+                changes.append(fullURL)
             else:
-                SDTMPackages.append(fullURL)
+                packages.append(fullURL)
 
     # Filter out packages/changelists that are before firstPackageDate (initially 2014 Q3)
     # This also extracts the date from the url to be used for sorting and to be used in the database
     # Final form is list with elements in format [date, link]
-    SDTMPackages = [[i.split("%20")[-1][:-4][0:10], i] for i in SDTMPackages if i.split("%20")[-1][:-4] >= firstPackageDate]
-    SDTMChanges = [[i.split("%20")[-1][:-4][0:10], i] for i in SDTMChanges if i.split("%20")[-1][:-4] >= firstPackageDate]
+    packages = [[i.split("%20")[-1][:-4][0:10], i] for i in packages if i.split("%20")[-1][:-4] >= firstPackageDate]
+    changes = [[i.split("%20")[-1][:-4][0:10], i] for i in changes if i.split("%20")[-1][:-4] >= firstPackageDate]
 
     # Sorts by extracted date mostly for the typo noted above, but helps ensure packages are read in chronologically
-    SDTMPackages.sort()
-    SDTMChanges.sort()
+    packages.sort()
+    changes.sort()
 
-    return SDTMPackages, SDTMChanges
+    return packages, changes
 
 
-# Loads the initial data for the capstone project (2014 Q3 - ~2019Q4 on assignment)
-# Because of how it is coded, it will look for every package up to present
+# Loads the initial data for a given standard (default SDTM from 2014 Q3 - ~2019Q4)
+# Because of how it is coded, it will look for every package up to present from firstPackageDate
 # It will take a long time, especially as more packages are added as it has at least O(n) complexity,
 # n being the number of terms, codelists, and changes for every package combined
-def initial_load():
-    # Reference variables
-    firstPackageDate = "2014-10-06"
-    archiveLink = "https://evs.nci.nih.gov/ftp1/CDISC/SDTM/Archive/"
+def initial_load(firstPackageDate = "2014-10-06", standard="SDTM"):
+    packages, changes = get_packages(firstPackageDate, standard)
 
-    SDTMPackages, SDTMChanges = get_packages(firstPackageDate, archiveLink)
-
-    for i in SDTMPackages:
-        read_data(i[0], i[1])
+    for i in packages:
+        read_data(i[0], i[1], standard)
         print(i[0], "package loaded")
     print("Packages loaded")
 
-    for i in SDTMChanges:
+    for i in changes:
         read_changes(i[0], i[1])
         print(i[0], "changes loaded")
     print("Changelists loaded")
 
 
-# Automatically updates the database with all recent packages/changelogs not in the database
-# Assumes that there is at least one package in the database
-def update_database():
+# Automatically updates the database with all recent packages/changelogs not in the database for a given standard
+# Assumes that there is at least one package in the database of that standard
+def update_database(standard):
     connection = sql.connect('CDISC.db')
 
     # Get the latest version date in the database
     latestVersion = connection.execute('SELECT Current_Version '
                                        'FROM Code '
+                                       'WHERE Standard = ? '
                                        'ORDER BY Current_Version DESC '
-                                       'LIMIT 1;').fetchall()[0][0]
+                                       'LIMIT 1;', (standard,)).fetchall()[0][0]
 
-    packages, changes = get_packages(latestVersion)
+    packages, changes = get_packages(latestVersion, standard)
 
     # Remove first element as that is already in the database
     packages = packages[1:]
@@ -312,7 +312,7 @@ def update_database():
     # If there are new packages, read into database
     if len(packages) > 0:
         for i in packages:
-            read_data(i[0], i[1])
+            read_data(i[0], i[1], standard)
             print(i[0], "package loaded")
         print("Packages updated")
 
@@ -338,4 +338,4 @@ def manual_load_data(date, package, changelog):
 
 # Debugging/Testing section
 if __name__ == "__main__":
-    pass
+    create_tables()
