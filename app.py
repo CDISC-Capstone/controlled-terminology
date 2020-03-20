4304310a634b322b28bc4e19f9da83a47c7f3101
Nelson <nelson.huang1998@gmail.com>
2020-03-19 22:59:01 -0400

    File load & installed Bokeh

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from functools import wraps
+import database
+import urllib
+from bokeh.plotting import figure, output_file, show
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+import requests
 import sys
 
 app = Flask(__name__)
@@ -7,11 +12,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    a = 5
-    b = 2
-    c = a + b
-    var = c + a + 2
-    return render_template('homepage.html', c=c, var=var)
+    firstPackageDate = "2014-10-06"
+    SDTM_packages, SDTM_changes = database.get_packages(firstPackageDate)
+
+    link = SDTM_packages[0][1]  # https://evs.nci.nih.gov/ftp1/CDISC/SDTM/Archive/SDTM%20Terminology%202014-10-06.txt
+    f = requests.get(link)
+
+    header = f.text.split('\n')[0]
+    case = []
+
+    for line in f.text.split('\n'):
+        for data in line.text.split('\t'):
+            case.append(data)
+
+    print(case, file=sys.stderr)
+
+    plot = figure()
+    plot.square([1, 2], [3, 4])
+    file = file_html(plot, CDN, "test")
+    show(plot)
+
+    return render_template('homepage.html', SDTM_packages=SDTM_packages, SDTM_changes=SDTM_changes)
+
 
 
 if __name__ == '__main__':
