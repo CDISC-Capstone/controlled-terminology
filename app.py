ebc5b060b955c974c5acd0faedfcc4bac9e0f7ac
Nelson <nelson.huang1998@gmail.com>
2020-04-02 12:49:04 -0400

    UI creation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-import database
+import query
+import sqlite3 as sql
 import urllib
 from bokeh.plotting import figure, output_file, show
 from bokeh.resources import CDN
@@ -12,25 +13,13 @@ host = 'http://127.0.0.1:5000/'
 
 @app.route('/')
 def home():
-    firstPackageDate = "2014-10-06"
-    SDTM_packages, SDTM_changes = database.get_packages(firstPackageDate, 'SDTM')
 
-    link = SDTM_packages[0][1]  # https://evs.nci.nih.gov/ftp1/CDISC/SDTM/Archive/SDTM%20Terminology%202014-10-06.txt
-    f = requests.get(link)
-    header = f.text.split('\n')[0].split("\t")
+    codes, terms = query.get_basic_data()
+    #CL_activeDates, CL_current, CL_changes = query.get_codelist_changes(code, startDate, endDate)
+    #term_activeDates, term_current, term_changes = query.get_term_changes(code, startDate, endDate)
 
-    case = []  # list type: contains each entry
-    for i in range(1, len(SDTM_packages)):
-        link = SDTM_packages[i][1]  # https://evs.nci.nih.gov/ftp1/CDISC/SDTM/Archive/SDTM%20Terminology%202014-10-06.txt
-        f = requests.get(link)
 
-        for line in f.text.split('\n'):
-            for data in line.split('\t'):
-                case.append(data)
-
-    print(case, file=sys.stderr)
-
-    return render_template('homepage.html', url=host, SDTM_packages=SDTM_packages, SDTM_changes=SDTM_changes)
+    return render_template('homepage.html', url=host)
 
 
 
