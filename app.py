fea5ab6871490f075e22b463baa026fc91cd6174
Perry Tsang <perry.tsang.98@gmail.com>
2020-04-04 13:31:48 -0400

    Added form checking to ensure fields are filled out

--- app.py
+++ app.py
@@ -30,40 +30,44 @@ def home():
         if termSubmitted is None:
             terms = query.get_term_data(codelist)
             list_of_terms = [(t[0], t[3]) for t in terms]
-            return render_template('homepage.html', url=host, list_of_codes=list_of_codes, terms=1, list_of_terms=list_of_terms)
+            return render_template('homepage.html', url=host, list_of_codes=list_of_codes, list_of_terms=list_of_terms)
+        # If term is selected, query changes for date range
         else:
             term = request.form['terms'].split()[0]
             startDate = request.form['start_date']
-            startDate = datetime.strptime(startDate, '%B %d, %Y').strftime('%Y-%d-%m')
             endDate = request.form['end_date']
-            endDate = datetime.strptime(endDate, '%B %d, %Y').strftime('%Y-%d-%m')
 
-            '''
-            CL_activeDates: [Creation Date, Deprecation Date]
-            CL_current: [Code, Extensible?, Name, Submission Value, Synonyms, Definition, NCI Preferred Term]
-            CL_changes: [Date, Code, Codelist, Term Type, Request Code, Change Type, Severity, Change Summary, Original, New,
-                        Change Instructions]
-            '''
-            CL_activeDates, CL_current, CL_changes = query.get_codelist_changes(codelist, startDate, endDate)
+            # Ensure both dates are selected
+            if startDate != '' and endDate != '':
+                startDate = datetime.strptime(startDate, '%B %d, %Y').strftime('%Y-%d-%m')
+                endDate = datetime.strptime(endDate, '%B %d, %Y').strftime('%Y-%d-%m')
 
-            '''
-            term_activeDates: [Creation Date, Deprecation Date]
-            term_current: [Codelist, Code, Submission Value, Synonyms, Definition, NCI Preferred Term]
-            term_changes: [Date, Code, Codelist, Term Type, Request Code, Change Type, Severity, Change Summary, Original, New,
-                        Change Instructions]
-            '''
-            term_activeDates, term_current, term_changes = query.get_term_changes(term, startDate, endDate)
+                '''
+                CL_activeDates: [Creation Date, Deprecation Date]
+                CL_current: [Code, Extensible?, Name, Submission Value, Synonyms, Definition, NCI Preferred Term]
+                CL_changes: [Date, Code, Codelist, Term Type, Request Code, Change Type, Severity, Change Summary, Original, New,
+                            Change Instructions]
+                '''
+                CL_activeDates, CL_current, CL_changes = query.get_codelist_changes(codelist, startDate, endDate)
 
-            print(codelist, term)
-            print(CL_activeDates)
-            print(CL_current)
-            print(CL_changes)
+                '''
+                term_activeDates: [Creation Date, Deprecation Date]
+                term_current: [Codelist, Code, Submission Value, Synonyms, Definition, NCI Preferred Term]
+                term_changes: [Date, Code, Codelist, Term Type, Request Code, Change Type, Severity, Change Summary, Original, New,
+                            Change Instructions]
+                '''
+                term_activeDates, term_current, term_changes = query.get_term_changes(term, startDate, endDate)
 
-            print(term_activeDates)
-            print(term_current)
-            print(term_changes)
+                print(codelist, term)
+                print(CL_activeDates)
+                print(CL_current)
+                print(CL_changes)
 
-    return render_template('homepage.html', url=host, list_of_codes=list_of_codes, terms=0)
+                print(term_activeDates)
+                print(term_current)
+                print(term_changes)
+
+    return render_template('homepage.html', url=host, list_of_codes=list_of_codes)
 
 
 if __name__ == '__main__':
