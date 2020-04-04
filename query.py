19ae04dc68998dd5b3f7e1ab17f1dac9fbbec228
Perry Tsang <perry.tsang.98@gmail.com>
2020-04-04 12:12:46 -0400

    Changed variable name from codes to codeslists in get_basic_data for clarity

--- query.py
+++ query.py
@@ -4,13 +4,13 @@ import sqlite3 as sql
 def get_basic_data():
     conn = sql.connect("CDISC.db")
 
-    codes = conn.execute('SELECT c.Code, c.Term_Type, c.Standard, cl.Submission_Value, cl.Name '
-                         'FROM Code c INNER JOIN Codelist cl ON c.Code = cl.Code;').fetchall()
+    codelists = conn.execute('SELECT c.Code, c.Term_Type, c.Standard, cl.Submission_Value, cl.Name '
+                             'FROM Code c INNER JOIN Codelist cl ON c.Code = cl.Code;').fetchall()
 
     terms = conn.execute('SELECT c.Code, c.Term_Type, c.Standard, t.Submission_Value '
                          'FROM Code c INNER JOIN Term t ON c.Code = t.Code;').fetchall()
     conn.close()
-    return codes, terms
+    return codelists, terms
 
 
 def get_codelist_changes(code, startDate, endDate):
